b57ebb21ee6bd627e1f529c5aa3f42456fca51c1
Angel <tvangelam@gmail.com>
2024-07-20 14:35:28 -0400

    exercice1.py

---
+++ exercice.py
@@ -0,0 +1,58 @@
+# EJERCICIO:
+
+# Crea un comentario en el código y coloca la URL del sitio web oficial del
+# lenguaje de programación que has seleccionado.
+
+# https://docs.python.org/3/
+
+
+# Representa las diferentes sintaxis que existen de crear comentarios
+# en el lenguaje (en una línea, varias...).
+
+# this is a comment and is is only in a single line
+
+
+ # Crea una variable (y una constante si el lenguaje lo soporta).
+a = 'fasfd'
+print(a)
+# this is a varible
+# there aren't constants
+
+
+# Crea variables representando todos los tipos de datos primitivos
+# del lenguaje (cadenas de texto, enteros, booleanos...).
+
+#Integer
+num1 = 1
+print(num1)
+
+#Float
+num2 = 1.25
+print(num2)
+
+#Complex
+num3 = 1 + 2j
+num4 = 2 + 3J
+print(num3,num4)
+
+#Boolean
+b = True
+c = False
+print(b,c)
+
+#String
+d = 'this is a string'
+e = "this is an another string"
+print(d,e)
+
+#Null
+f = None
+print(f)
+
+
+# Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+print('Hi, Python')
+
+# ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y
+# debemos comenzar por el principio.
+ 
