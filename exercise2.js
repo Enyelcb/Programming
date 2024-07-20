d487c05d3be5f298340127f685a24f777f951303
Angel <tvangelam@gmail.com>
2024-07-20 18:12:52 -0400

    Update exercise2.js

--- exercise2.js
+++ exercise2.js
@@ -3,6 +3,49 @@
      Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits...
      (Ten en cuenta que cada lenguaje puede poseer unos diferentes) */
 
+// Assignment
+var a = 10;
+console.log(a);
+
+//Addition
+var b = 1;
+var c = a+b;
+console.log(c);
+
+//Subtraction
+var d = a+b;
+console.log(d);
+
+//Multiplication
+var e = a*b;
+console.log(e);
+
+//Exponetiation
+var f = a**b;
+console.log(f);
+
+//Division
+var g = a/b;
+console.log(g);
+
+//Modulus
+var h = a%b;
+console.log(h);
+
+//Increment
+var i = a++
+console.log(i);
+
+//Decrement
+var j = b--;
+
+
+
+
+
+
+
+
  /* Utilizando las operaciones con operadores que tú quieras, crea ejemplos
   que representen todos los tipos de estructuras de control que existan
   en tu lenguaje:
